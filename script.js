cb2f754b94054d53cdebc40dddaebbf86dc328d5
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-11-18 17:54:42 +0300

    feat: add js

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let timer = document.getElementById('timer'),
-        years = timer.querySelector('.years'),
-       
-        days = timer.querySelector('.days'),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds');
-
-    const wed = '2019-03-02T19:30:00.000Z',
+    const timer = document.getElementById('timer'),
+        yearsT = timer.querySelector('.yearsTitle'),
+        daysT = timer.querySelector('.daysTitle'),
+        hoursT = timer.querySelector('.hoursTitle'),
+        minutesT = timer.querySelector('.minutesTitle'),
+        secondsT = timer.querySelector('.secondsTitle'),
+        yearsSub = timer.querySelector('.yearsSubtitle'),
+        daysSub = timer.querySelector('.daysSubtitle'),
+        hoursSub = timer.querySelector('.hoursSubtitle'),
+        minutesSub = timer.querySelector('.minutesSubtitle'),
+        secondsSub = timer.querySelector('.secondsSubtitle'),
+        wed = 'Sat, 02 Mar 2019 19:30:00 +03',
         structTime = (t) => {
             let seconds = Math.floor((t / 1000) % 60),
                 minutes = Math.floor((t / 1000 / 60) % 60),
                 hours = Math.floor((t / 1000 / 60 / 60) % 24),
-                days = Math.floor(t / (1000 * 60 * 60 * 24)),
-                
-                years = Math.floor(t / (1000 * 60 * 60 * 24 * 365));
-
+                days = Math.floor(t / (1000 * 60 * 60 * 24) % 365.242),
+                years = Math.floor(t / (1000 * 60 * 60 * 24 * 365.242));
             return {
                 years: years,
-
                 days: days,
                 hours: hours,
                 minutes: minutes,
                 seconds: seconds
             };
-        };
+        },
+        declensionSub = (num, words) => words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
 
     function updateClock() {
         let currentDate = Date.parse(new Date()),
             pastDays = (currentDate - Date.parse(wed)),
             res = structTime(pastDays);
 
-        years.textContent = res.years;
-   
-        days.textContent = res.days;
-        hours.textContent = res.hours;
-        minutes.textContent = res.minutes;
-        seconds.textContent = res.seconds;
+        yearsT.textContent = res.years;
+        daysT.textContent = res.days;
+        hoursT.textContent = res.hours < 10 ? "0" + res.hours : res.hours;
+        minutesT.textContent = res.minutes < 10 ? "0" + res.minutes : res.minutes;
+        secondsT.textContent = res.seconds < 10 ? "0" + res.seconds : res.seconds;
 
-        if (res.hours < 10) {
-            hours.textContent = "0" + res.hours;
-        }
-        if (res.minutes < 10) {
-            minutes.textContent = "0" + res.minutes;
-        }
-        if (res.seconds < 10) {
-            seconds.textContent = "0" + res.seconds;
-        }
-
-    }
+        if (res.years === 1) yearsSub.textContent = "ГОД";
+        if (res.years > 1 && res.years < 5) yearsSub.textContent = "ГОДА";
+        daysSub.textContent = declensionSub(res.days, ['ДЕНЬ', 'ДНЯ', 'ДНЕЙ']);
+        hoursSub.textContent = declensionSub(res.hours, ['ЧАС', 'ЧАСА', 'ЧАСОВ']);
+        minutesSub.textContent = declensionSub(res.minutes, ['МИНУТА', 'МИНУТЫ', 'МИНУТ']);
+        secondsSub.textContent = declensionSub(res.seconds, ['СЕКУНДА', 'СЕКУНДЫ', 'СЕКУНД']);
+        // if (res.seconds === 1 || res.seconds === 21 || res.seconds === 31 || res.seconds === 41 || res.seconds === 51) {
+        //     secondsSub.textContent = " СЕКУНДА";
+        // } else if (res.seconds > 1 && res.seconds < 5 || res.seconds > 21 && res.seconds < 25 || res.seconds > 31 && res.seconds < 35 || res.seconds > 41 && res.seconds < 45 || res.seconds > 51 && res.seconds < 55) {
+        //     secondsSub.textContent = " СЕКУНДЫ";
+        // } else { secondsSub.textContent = " СЕКУНД" }
+    };
     setInterval(updateClock, 1000);
-
-
-
 })
